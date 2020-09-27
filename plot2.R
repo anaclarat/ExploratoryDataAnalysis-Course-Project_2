f3579487a0e0a751ae47d0dfbcd66c764bb98fd9
Ana Clara <anaclarat@gmail.com>
2020-09-27 19:56:47 -0400

    Adding second plot

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
-#Plot 1 - 1.	Have total emissions from PM2.5 decreased in the United States 
-#from 1999 to 2008? Using the base plotting system, make a plot showing the total
-#PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+#Plot 2 - 2.2.	Have total emissions from PM2.5 decreased in the Baltimore City,
+#Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to 
+#make a plot answering this question.
 
 #Setting working directory
 setwd('D:/DataScienceSpecialization/4-Exploratory_Data_Analysis/Week4/Assignment/ExploratoryDataAnalysis-Course-Project_2')
@@ -17,26 +17,28 @@ rm(url)
 if (!file.exists('./data/exdata_data_NEI_data'))
 {unzip('./data/data.zip',exdir = './data')}
 
-#Reading data and manipulating it
+#Reading data,subsetting and manipulating it
 NEI <- readRDS("./data/summarySCC_PM25.rds")
 #SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+NEI <- subset(NEI,fips == "24510")
 
 ToTal_Emissions_grouped_year <- NEI %>% group_by(year) %>% 
   summarise(Emissions=sum(Emissions)/1000)
 
 #Plotting
-png(filename = 'plot1.png',bg= 'transparent', width=600, height=480)
+png(filename = 'plot2.png',bg= 'transparent', width=700, height=480)
 
 plot(ToTal_Emissions_grouped_year,xlab = "Years",
      ylab = expression('PM'[2.5]*' Emissions in kilotons'),type='b',col= 'black',
-     ylim=c(min(ToTal_Emissions_grouped_year$Emissions)-1000,
-            max(ToTal_Emissions_grouped_year$Emissions)+1000),
+     ylim=c(min(ToTal_Emissions_grouped_year$Emissions-1),
+            max(ToTal_Emissions_grouped_year$Emissions)+1),
      xlim=c(1998,2008))
 
 text(x=c(1999,2002,2005,2008), y = round(ToTal_Emissions_grouped_year$Emissions,2), 
      label = round(ToTal_Emissions_grouped_year$Emissions,2), pos=3,
      cex = .8, col = "red")
-mtext(expression('Have total emissions from PM'[2.5]*' decreased in the United States from 1999 to 2008?'),
+mtext(expression('Have total emissions from PM'[2.5]*' decreased in the Baltimore City,Maryland from 1999 to 2008?'),
       cex=1.2)
 
 dev.off()
